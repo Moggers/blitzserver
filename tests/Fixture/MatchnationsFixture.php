08f8c623b55e00561751adf5cccf5df38c641d81
matt kramara <mattkramara@gmail.com>
2016-02-08 16:56:52 +0800

    Rebaked matchnations

--- tests/Fixture/MatchnationsFixture.php
+++ tests/Fixture/MatchnationsFixture.php
@@ -20,8 +20,13 @@ class MatchnationsFixture extends TestFixture
         'matchid' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
         'nationid' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
         'markdelete' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        '_indexes' => [
+            'nationid' => ['type' => 'index', 'columns' => ['nationid'], 'length' => []],
+        ],
         '_constraints' => [
             'primary' => ['type' => 'primary', 'columns' => ['matchid', 'nationid'], 'length' => []],
+            'matchnations_ibfk_1' => ['type' => 'foreign', 'columns' => ['matchid'], 'references' => ['matches', 'id'], 'update' => 'restrict', 'delete' => 'restrict', 'length' => []],
+            'matchnations_ibfk_2' => ['type' => 'foreign', 'columns' => ['nationid'], 'references' => ['nations', 'id'], 'update' => 'restrict', 'delete' => 'restrict', 'length' => []],
         ],
         '_options' => [
             'engine' => 'InnoDB',
