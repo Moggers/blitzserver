756ab5ac549d96d7e4dd396399ec3764dda4f05a
Matt <mattkramara@gmail.com>
2020-12-23 16:53:01 +1000

    Select list for research difficulty

--- .build/deploy.sh
+++ .build/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+cd /home/dom5/blitzserver
 TAG=$(git tag --points-at HEAD)
 if [[ ${#TAG} > 0 ]]; then
-	cd /home/dom5/blitzserver
 	git pull
 	~/.cargo/bin/diesel migration run
 	cargo build
