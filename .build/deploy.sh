6c7188fda2265c8c9b544992347ac2af27511816
Matt <mattkramara@gmail.com>
2020-12-23 15:38:05 +1000

    On deploy tagged stuff

--- .build/deploy.sh
+++ .build/deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-cd /home/dom5/blitzserver
-git pull
-~/.cargo/bin/diesel migration run
-cargo build
-systemctl --user restart blitzserver
+TAG=$(git tag --points-at HEAD)
+if [[ ${#TAG} > 0 ]]; then
+	cd /home/dom5/blitzserver
+	git pull
+	~/.cargo/bin/diesel migration run
+	cargo build
+	systemctl --user restart blitzserver
+fi
