d812f2332f713890af1061b0cd1046ddf3c58f59
Matt <mattkramara@gmail.com>
2020-12-24 13:18:08 +1000

    Move version check to CI

--- .build/deploy.sh
+++ .build/deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 cd /home/dom5/blitzserver
-TAG=$(git tag --points-at HEAD)
-if [[ ${#TAG} > 0 ]]; then
-	git pull
-	~/.cargo/bin/diesel migration run
-	cargo build
-	systemctl --user restart blitzserver
-fi
+git pull
+~/.cargo/bin/diesel migration run
+cargo build
+systemctl --user restart blitzserver
