f0ad016e5e4283da6906d90ef9c9ef8fce618287
Matt <mattkramara@gmail.com>
2021-07-10 02:03:05 +1000

    Second attempty mystery bytes

--- src/packets/submit2h_req.rs
+++ src/packets/submit2h_req.rs
@@ -3,7 +3,9 @@ use crate::files::saves::utils::ReadDom5Ext;
 #[derive(Clone)]
 pub struct Submit2hReq {
     pub nation_id: u8,
-    pub unk: [Vec<u8>; 4],
+    pub unk1: u8,
+    pub unk2: Vec<u8>,
+    pub unk3: [u8; 5],
     pub twoh_contents: Vec<u8>,
 }
 
@@ -11,7 +13,9 @@ impl std::fmt::Debug for Submit2hReq {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("Submit2hReq")
             .field("nation_id", &self.nation_id)
-            .field("unk", &self.unk)
+            .field("unk1", &self.unk1)
+            .field("unk2", &self.unk2)
+            .field("unk3", &self.unk3)
             .field(
                 "twoh_contents[0..32]",
                 &self
@@ -28,17 +32,17 @@ impl std::fmt::Debug for Submit2hReq {
 impl Submit2hReq {
     pub fn from_reader<R: std::io::Read>(r: &mut R) -> Submit2hReq {
         let nation_id = r.read_u8().unwrap();
-        let unk = [
-            r.read_domu8vec_nt().unwrap(),
-            r.read_domu8vec_nt().unwrap(),
-            r.read_domu8vec_nt().unwrap(),
-            r.read_domu8vec_nt().unwrap()
-        ];
+        let unk1 = r.read_u8().unwrap();
+        let unk2 = r.read_domu8vec_nt().unwrap();
+        let mut unk3 = [0u8; 5];
+        r.read_exact(&mut unk3).unwrap();
         let mut twoh_contents = vec![];
         r.read_to_end(&mut twoh_contents).unwrap();
         Submit2hReq {
             nation_id,
-            unk,
+            unk1,
+            unk2,
+            unk3,
             twoh_contents,
         }
     }
